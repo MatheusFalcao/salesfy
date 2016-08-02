c355c9d4c77d53e9e5b82a97e95be3e3fda2ab8d
Matheus Falcão Caldas de Souza <maatheusfalcao@gmail.com>
2016-08-02 17:51:49 +0000

    fix active admin assets on heroku

--- config/application.rb
+++ config/application.rb
@@ -7,11 +7,6 @@ require 'rails/all'
 #Bundler.require(*Rails.groups)
 Bundler.require(:default, :assets, Rails.env)
 
-#Added to fix devise/active admin issue ?
-config.assets.initialize_on_precompile = false
-
-# Precompile additional assets. Defaults to [application.js, application.css, non-JS/CSS]
-config.assets.precompile += ['active_admin.css.scss', 'active_admin.js'] 
 
 module Fdcwebapp
   class Application < Rails::Application
